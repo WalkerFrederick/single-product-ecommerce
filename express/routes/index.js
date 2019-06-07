5fe49dca207eef1ef0ce9b14ebb3d9dc82d02e60
Walker Frederick <walker@urdev.xyz>
2019-06-07 13:41:00 -0400

    initital commit

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('things');
+/* GET products */
+router.get('/products', function(req, res, next) {
+  res.json([
+    {productName: 'urphone',
+     productDesc: 'A great phone that will knock your socks off',
+    productPrice: 150},
+    {productName: 'urphone plus',
+     productDesc: 'A great phone that will knock your socks off again',
+    productPrice: 150},
+    {productName: 'urphone case',
+     productDesc: 'A great phone case that will knock your socks off',
+    productPrice: 150},
+  ]);
 });
 
 module.exports = router;
