74b0206a9f3905b7d8d4526959ff90ae4978c751
Walker Frederick <walker@urdev.xyz>
2019-06-08 10:33:29 -0400

    moved react app into express directory

--- express/routes/index.js
+++ express/routes/index.js
@@ -5,15 +5,27 @@ var router = express.Router();
 router.get('/products', function(req, res, next) {
   res.json([
     {productName: 'urphone',
+     productType: 'phone',
      productDesc: 'A great phone that will knock your socks off',
     productPrice: 150},
     {productName: 'urphone plus',
+    productType: 'phone',
      productDesc: 'A great phone that will knock your socks off again',
     productPrice: 150},
     {productName: 'urphone case',
-     productDesc: 'A great phone case that will knock your socks off',
-    productPrice: 150},
+    productType: 'case',
+    productDesc: 'A great phone case that will knock your socks off',
+    productPrice: 12},
+    {productName: 'urphone case plus',
+    productType: 'case',
+    productDesc: 'A great phone case that will knock your socks off',
+    productPrice: 19},
   ]);
+});
+
+router.get('/phoneimage', function(req, res, next) {
+  console.log(__dirname + '\\images/phone.png')
+  res.sendFile(__dirname + `\\images\\phone.png`)
 });
 
 module.exports = router;
